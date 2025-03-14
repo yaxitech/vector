9fcb0a3dcdb33184bf9f81d690d07d4e1284f2e8
Vincent Haupert <vincent.haupert@yaxi.tech>
2025-03-14 09:57:22 +0100

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,90 @@
+{
+  description = "A lightweight, ultra-fast tool for building observability pipelines";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane, ... }:
+    let
+      cargoTOML = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+      name = cargoTOML.package.name;
+
+      pkgsFor = system: import nixpkgs {
+        inherit system;
+        overlays = [
+          rust-overlay.overlays.default
+
+          (final: prev: {
+            rustToolchain = final.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+            craneLib = (crane.mkLib prev).overrideToolchain final.rustToolchain;
+          })
+        ];
+      };
+
+      lib = nixpkgs.lib;
+    in
+    lib.foldl lib.recursiveUpdate { } [
+      (flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          packages.default = pkgs.craneLib.buildPackage rec {
+            pname = "vector";
+            src = self;
+
+            buildInputs = with pkgs; [
+              openssl
+            ];
+            nativeBuildInputs = with pkgs; [
+              git
+              perl
+              pkg-config
+              rustPlatform.bindgenHook
+            ];
+
+            cargoExtraArgs = "-F api,api-client,sinks-azure_monitor_logs_dce,sinks-console,sinks-prometheus,sources-file,sources-host_metrics,sources-journald,sources-prometheus-scrape,transforms-remap --no-default-features";
+            doCheck = false;
+
+            PROTOC = "${pkgs.protobuf}/bin/protoc";
+            PROTOC_INCLUDE = "${pkgs.protobuf}/include";
+
+            meta = with lib; {
+              description = "A lightweight, ultra-fast tool for building observability pipelines";
+              homepage = "https://vector.dev/";
+              license = licenses.asl20;
+              mainProgram = pname;
+            };
+          };
+
+          devShells.default = pkgs.mkShell {
+            name = "${name}-dev-shell";
+
+            nativeBuildInputs = with pkgs; [
+              rustToolchain
+            ];
+
+            buildInputs = with pkgs; [
+              protobuf
+            ];
+          };
+        }))
+
+      {
+        overlays.default = final: _prev: {
+          vector = self.packages.${final.system}.default;
+        };
+      }
+    ];
+}
